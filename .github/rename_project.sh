7c5688ba61b4fff6bcafb4cea94efb15a95730ac
RobinAxelsson <RobinAxelsson@users.noreply.github.com>
2022-12-06 17:12:25 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="RobinAxelsson"
+original_name="pytesttemplate"
+original_urlname="PyTestTemplate"
+original_description="Awesome pytesttemplate created by RobinAxelsson"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv pytesttemplate $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
